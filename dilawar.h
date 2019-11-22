db4687aeda146e888b3ce101dc99ccc932ffa292
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-11-22 11:33:24 +0530

    Added a top-level file.

---
+++ dilawar.h
@@ -0,0 +1,25 @@
+// =====================================================================================
+//
+//       Filename:  dilawar.h
+//
+//    Description: Top-level header. 
+//
+//        Version:  1.0
+//        Created:  11/22/2019 11:31:47 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Dilawar Singh (), dilawar.s.rajput@gmail.com
+//   Organization:  NCBS Bangalore
+//
+// =====================================================================================
+
+#ifndef DILAWAR_H
+#define DILAWAR_H
+
+#include "random/random.hpp"
+#include "utility/io.hpp"
+#include "external/gnuplot-iostream.h"
+#include "external/prettyprint.hpp"
+
+#endif /* end of include guard: DILAWAR_H */
