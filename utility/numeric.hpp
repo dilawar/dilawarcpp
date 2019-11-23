fe4000bd4def0e029a97db1eb894bd2f6c5f5029
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-11-23 12:33:08 +0530

    added numeric library.

---
+++ utility/numeric.hpp
@@ -0,0 +1,49 @@
+// =====================================================================================
+//
+//       Filename:  numeric.hpp
+//
+//    Description:
+//
+//        Version:  1.0
+//        Created:  11/23/2019 12:30:55 PM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Dilawar Singh (), dilawar.s.rajput@gmail.com
+//   Organization:  NCBS Bangalore
+//
+// =====================================================================================
+
+#include <numeric>
+using namespace std;
+
+namespace dilawar {
+
+// These are from Knuth and from here
+// https://stackoverflow.com/a/253874/1805129
+
+template <typename T = double>
+bool approximatelyEqual(T a, T b, T epsilon)
+{
+    return fabs(a - b) <= ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+template <typename T = double>
+bool essentiallyEqual(T a, T b, T epsilon)
+{
+    return fabs(a - b) <= ((fabs(a) > fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+template <typename T = double>
+bool definitelyGreaterThan(T a, T b, T epsilon)
+{
+    return (a - b) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+template <typename T = double>
+bool definitelyLessThan(T a, T b, T epsilon)
+{
+    return (b - a) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+}  // namespace dilawar
